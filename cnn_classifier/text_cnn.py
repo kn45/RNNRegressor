7065cd8073c95debdc85608ef357c497d766b0cf
kn45 <381404825@qq.com>
2017-07-26 18:06:08 +0800

    rough cnn

--- cnn_classifier/text_cnn.py
+++ cnn_classifier/text_cnn.py
@@ -8,13 +8,27 @@ class TextCNNClassifier(object):
     """CNN for text classifier
     """
     def __init__(self, seq_len=100, emb_dim=256, nclass=1, vocab_size=10000,
-                 filter_sizes=None, nfilters=3, reg_lambda=0.0, lr=1e-3):
+                 filter_sizes=None, nfilters=3, reg_lambda=0.0, lr=1e-3,
+                 nlabel=1, label_repr='dense'):
+        """Construct CNN network.
+        nlabel > 1 only when multi-label task.
+        """
+        if label_repr not in ['dense', 'sparse']:
+            raise Exception('invalid label_repr')
 
         # prepare input and output placeholder
         self.inp_x = tf.placeholder(tf.int32, [None, seq_len], name='input_x')
         self.inp_y = tf.placeholder(tf.float32, [None, nclass], name='input_y')
         self.dropout_prob = tf.placeholder(tf.float32, name='dropout_prob')
-
+        self.label_repr = label_repr
+        if label_repr == 'dense':
+            self.inp_y_dense = tf.placeholder(tf.float32, [None, nclass], 'input_y')
+            self.inp_y_sparse = tf.argmax(self.inp_y_dense, 1)
+        if label_repr == 'sparse':
+            self.inp_y_sparse = tf.placeholder(tf.int64, [None, nlabel], 'input_y')
+            self.inp_y_dense = tf.reduce_sum(
+                tf.one_hot(indices=self.inp_y_sparse, depth=nclass),
+                reduction_indices=1)
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
 
         # embedding
@@ -75,13 +89,13 @@ class TextCNNClassifier(object):
             self.l2_loss = tf.nn.l2_loss(W) + tf.nn.l2_loss(b)
             self.scores = tf.nn.xw_plus_b(self.h_drop, W, b, name='scores')
             self.preds = tf.argmax(self.scores, 1, name='predictions')
-            # self.pred_proba =
+            self.proba = tf.nn.softmax(self.scores)
 
         # calculate mean cross-entropy loss
         with tf.name_scope('loss'):
             self.loss = tf.reduce_mean(
                 tf.nn.softmax_cross_entropy_with_logits(
-                    logits=self.scores, labels=self.inp_y))
+                    logits=self.scores, labels=self.inp_y_dense))
             self.total_loss = self.loss + reg_lambda * self.l2_loss
 
         with tf.name_scope('opt'):
@@ -92,29 +106,59 @@ class TextCNNClassifier(object):
         # accuracy
         with tf.name_scope('accuracy'):
             correct_preds = tf.equal(
-                self.preds, tf.argmax(self.inp_y, 1))
+                self.preds, self.inp_y_sparse)
             self.accuracy = tf.reduce_mean(
                 tf.cast(correct_preds, 'float'), name='accuracy')
 
-    def train_step(self, sess, inp_batch_x, inp_batch_y):
+        # auc
+        labels_c = self.inp_y_sparse
+        preds_c = self.proba[:, 1]
+        self.auc = tf.metrics.auc(
+            labels=labels_c,
+            predictions=preds_c,
+            num_thresholds=1000)
+
+        # saver and loader
+        self.saver = tf.train.Saver()
+
+    def train_step(self, sess, inp_batch_x, inp_batch_y, evals=None):
         input_dict = {
             self.inp_x: inp_batch_x,
-            self.inp_y: inp_batch_y,
             self.dropout_prob: 0.5}
+        if self.label_repr == 'dense':
+            input_dict[self.inp_y_dense] = inp_batch_y
+        if self.label_repr == 'sparse':
+            input_dict[self.inp_y_sparse] = inp_batch_y
         sess.run(self.opt, feed_dict=input_dict)
 
-    def eval_step(self, sess, inp_batch_x, inp_batch_y):
-        input_dict = {
-            self.inp_x: inp_batch_x,
-            self.inp_y: inp_batch_y,
+    def eval_step(self, sess, dev_x, dev_y, metrics=None):
+        if not metrics:
+            metrics = ['loss']
+        eval_dict = {
+            self.inp_x: dev_x,
             self.dropout_prob: 1.0}
-        loss, accuracy = sess.run(
-            [self.loss, self.accuracy], feed_dict=input_dict)
-        return loss, accuracy
+        if self.label_repr == 'dense':
+            eval_dict[self.inp_y_dense] = dev_y
+        if self.label_repr == 'sparse':
+            eval_dict[self.inp_y_sparse] = dev_y
+        eval_res = []
+        for metric in metrics:
+            if metric == 'loss':
+                eval_res.append(sess.run(self.loss, feed_dict=eval_dict))
+            if metric == 'accuracy':
+                eval_res.append(sess.run(self.accuracy, feed_dict=eval_dict))
+            if metric == 'auc':
+                eval_res.append(sess.run(self.auc, feed_dict=eval_dict)[0])
+        return eval_res
 
     def predict(self, sess, input_x):
-        pred_dict = {self.inp_x: input_x}
+        pred_dict = {
+            self.inp_x: input_x,
+            self.dropout_prob: 1.0}
         return sess.run(self.preds, feed_dict=pred_dict)
 
     def predict_proba(self, sess, input_x):
-        pred_dict = {self.inp_x: input_x}
+        pred_dict = {
+            self.inp_x: input_x,
+            self.dropout_prob: 1.0}
+        return sess.run(self.proba, feed_dict=pred_dict)

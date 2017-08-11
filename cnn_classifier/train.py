1863a81f812be709191418a017feadf3f35a4532
kn45 <381404825@qq.com>
2017-08-11 19:25:49 +0800

    update due to model

--- cnn_classifier/train.py
+++ cnn_classifier/train.py
@@ -10,33 +10,27 @@ import dataproc
 NCLASS = 2
 NWORDS = 18765
 SEQ_LEN = 100
-LABEL_REPR = 'sparse'
 FILTER_SIZES = [3, 4, 5]
 NFILTERS = 128
 
 
-def inp_fn(data):
-    def _inp_fn(data, y_repr='dense'):
-        inp_x = []
-        inp_y = []
-        for inst in data:
-            flds = inst.split('\t')
-            label = map(int, flds[0:1])
-            feats = map(int, flds[1:])
-            if y_repr == 'dense':
-                inp_y.append(dataproc.sparse2dense(label, ndim=NCLASS))
-            elif y_repr == 'sparse':
-                inp_y.append(label)
-            inp_x.append(dataproc.zero_padding(feats, SEQ_LEN))
-        return np.array(inp_x), np.array(inp_y)
-    return _inp_fn(data, LABEL_REPR)
+def inp_fn_unilabel(data):
+    inp_x = []
+    inp_y = []
+    for inst in data:
+        flds = inst.split('\t')
+        label = map(int, flds[0:1])
+        feats = map(int, flds[1:])
+        inp_y.append(label)
+        inp_x.append(dataproc.zero_padding(feats, SEQ_LEN))
+    return np.array(inp_x), np.array(inp_y)
 
 train_file = './rt-polarity.shuf.train'
 test_file = './rt-polarity.shuf.test'
 freader = dataproc.BatchReader(train_file)
 with open(test_file) as f:
     test_data = [x.rstrip('\n') for x in f.readlines()]
-test_x, test_y = inp_fn(test_data)
+test_x, test_y = inp_fn_unilabel(test_data)
 
 mdl = TextCNNClassifier(
     seq_len=SEQ_LEN,
@@ -47,12 +41,12 @@ mdl = TextCNNClassifier(
     nfilters=NFILTERS,
     reg_lambda=0.0,
     lr=1e-3,
-    label_repr=LABEL_REPR)
+    multi_label=False)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 sess.run(tf.local_variables_initializer())
-metrics = ['loss', 'auc']
+metrics = ['loss', 'accuracy', 'auc']
 niter = 0
 mdl_ckpt_dir = './model_ckpt/model.ckpt'
 while niter < 1000:
@@ -60,12 +54,18 @@ while niter < 1000:
     batch_data = freader.get_batch(128)
     if not batch_data:
         break
-    train_x, train_y = inp_fn(batch_data)
+    train_x, train_y = inp_fn_unilabel(batch_data)
     mdl.train_step(sess, train_x, train_y)
     train_eval = mdl.eval_step(sess, train_x, train_y, metrics)
     test_eval = mdl.eval_step(sess, test_x, test_y, metrics) \
-        if niter % 20 == 0 else 'SKIP'
+        if niter % 50 == 0 else 'SKIP'
     print niter, 'train:', train_eval, 'test:', test_eval
 save_path = mdl.saver.save(sess, mdl_ckpt_dir, global_step=mdl.global_step)
 print "model saved:", save_path
+
+with open('train_done_test_res', 'w') as f:
+    preds = mdl.predict_proba(sess, test_x)
+    for l, p in zip(test_y, preds):
+        print >> f, '\t'.join(map(str, [l[0], p[1]]))
+
 sess.close()
